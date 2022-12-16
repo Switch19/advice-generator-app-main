eee781ad768764819713b1bdabafe4e5d06bb282
SwitchTega <eokoloho@gmail.com>
2022-12-16 18:50:11 +0100

    final commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const quoteP = document.querySelector("#advice-text")
+const imgButton = document.querySelector(".dice")
+const adviceId = document.getElementById("advice-id")
+
+const fetchAdvice = async () => {
+    try {
+        const resp = await fetch('https://api.adviceslip.com/advice')
+        const data = await resp.json()
+        renderAdvice(data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+const renderAdvice = (adviceData) => {
+    quoteP.textContent = `"${adviceData.slip.advice}"`
+    adviceId.textContent = adviceData.slip.id
+}
+imgButton.addEventListener("click", fetchAdvice)
+document.body.onload = fetchAdvice;
+// fetch('https://api.adviceslip.com/advice').then((data)=> {
+//     // console.log(data)
+//     return data.json();
+// }).then((completedata) => {
+//     const completeAdvice = (completedata.slip.advice);
+//     const completeId = (completedata.slip.id)
+//     console.log(completeAdvice)
+//     console.log(completeId)
+//     body.addEventListener("load",() => {
+//         quoteP.innerHTML = completeAdvice
+//         adviceId.innerHTML = completeId    
+//     })
+    
+//     quoteP.innerHTML = completeAdvice
+//     adviceId.innerHTML = completeId
+// }).catch((error) => {
+//     console.log(error)
+// })
